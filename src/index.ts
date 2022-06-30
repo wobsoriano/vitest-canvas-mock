af28a1beeb5b10816d37c0c74bcadf9920931baf
Robert Soriano <sorianorobertc@gmail.com>
2022-06-30 09:44:38 -0700

    feat: add jest-canvas-mock compat

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-export const one = 1
-export const two = 2
+import { afterAll, vi } from 'vitest'
+// @ts-expect-error: Global jest
+global.jest = vi
+// @ts-expect-error: Missing files
+// eslint-disable-next-line import/first
+import getCanvasWindow from 'jest-canvas-mock/lib/window'
+
+const apis = [
+  'Path2D',
+  'CanvasGradient',
+  'CanvasPattern',
+  'CanvasRenderingContext2D',
+  'DOMMatrix',
+  'ImageData',
+  'TextMetrics',
+  'ImageBitmap',
+  'createImageBitmap',
+] as const
+
+const canvasWindow = getCanvasWindow({ document: window.document })
+
+apis.forEach((api) => {
+  global[api] = canvasWindow[api]
+  global.window[api] = canvasWindow[api]
+})
+
+afterAll(() => {
+  // @ts-expect-error: Global jest
+  delete global.jest
+  // @ts-expect-error: Global jest
+  delete global.window.jest
+})
+
+export {}
